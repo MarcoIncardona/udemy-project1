da09b1c1fd24f522868693d2e79335b6c47e660a
marco <incardonamarco0@gmail.com>
2023-08-08 16:04:10 +0200

    piccolo fix

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import { Component, OnChanges, OnDestroy, OnInit, SimpleChanges} from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -9,34 +9,38 @@ import { Subscription } from 'rxjs';
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
-export class RecipesListComponent implements OnInit, OnChanges{
+export class RecipesListComponent implements OnInit, OnDestroy{
   recipes!: Recipe[]
   id!: number
-
+  private recipeSub!: Subscription
+  
   ngOnInit(){
-    this.recipeService.onFetchData()
     this.recipes = this.recipeService.getRecipes()
-    this.recipeService.onRecipeChanges.subscribe(newRecipes => {
+    this.recipeService.onFetchData()   
+    this.recipeSub = this.recipeService.onRecipeChanges.subscribe(newRecipes => {
       this.recipes = newRecipes
-      console.log(newRecipes)
     })
   }
-
+  
+  
+  
+  
   constructor(private recipeService: RecipeService, private router: Router, private route: ActivatedRoute){
-
+    
   }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log(changes['recipes'])
-  }
-
+  
+  
   onRecipeSelect(recipe:Recipe){
     this.id = recipe.id
     this.router.navigate(["/recipes", this.id], {relativeTo: this.route})
   }
-
+  
   onRecipeEdit(){
     this.router.navigate(["/recipes", this.id, "edit"], {relativeTo: this.route})
+  }
+  
+  ngOnDestroy(): void {
+    this.recipeSub.unsubscribe()
   }
 
 }
